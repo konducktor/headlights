2bcc1ab63023653810243b68acd5e992534d7f6f
Andrei Pogorelov <adpogorelov@edu.hse.ru>
2026-07-23 17:00:33 +0300

    Dash, new visuals

--- player/player.gd
+++ player/player.gd
@@ -2,6 +2,12 @@ extends CharacterBody2D
 class_name Player
 
 
+enum PlayerState {
+	DEFAULT, DASH, DIE
+}
+
+var player_state: PlayerState
+
 @export_group("Movement")
 @export var max_speed: float
 @export var time_to_max_speed: float
@@ -11,19 +17,49 @@ const _TARGET: float = 0.99
 var _acceleration: float
 var _friction: float
 
+@export_group("Dash")
+@export var dash_speed: float
+
+var dash_timer: Timer
+var dash_direction: Vector2
+
 var _attack_component: AttackComponent
 
 
 func _ready() -> void:
 	_attack_component = $AttackComponent
+	
+	dash_timer = $DashTimer
+	dash_timer.timeout.connect(_on_dash_timer_timeout)
+	
+	player_state = PlayerState.DEFAULT
 
 
 func _physics_process(delta: float) -> void:
-	move_player(delta)
 	point_weapon_in_direction(delta)
+	
+	var movement_direction: Vector2 = Input.get_vector(
+		"player_left", "player_right", "player_up", "player_down")
+	
+	match player_state:
+		PlayerState.DEFAULT:
+			move_player(movement_direction, delta)
+			
+			if Input.is_action_just_pressed("player_dash"):
+				dash_direction = Vector2.RIGHT
+				if movement_direction != Vector2.ZERO:
+					dash_direction = movement_direction
+				
+				player_state = PlayerState.DASH
+				dash_timer.start()
+		
+		PlayerState.DASH:
+			velocity = dash_direction * dash_speed
+	
+	move_and_slide()
 
 
-func point_weapon_in_direction(delta: float) -> void:
+func point_weapon_in_direction(_delta: float) -> void:
 	var attack_direction: Vector2 = (get_global_mouse_position() - global_position).normalized()
 	_attack_component.point_in_direction(attack_direction)
 	
@@ -31,22 +67,24 @@ func point_weapon_in_direction(delta: float) -> void:
 		_attack_component.fire()
 
 
-func move_player(delta: float) -> void:
+func move_player(direction: Vector2, delta: float) -> void:
 	_acceleration = -log(1.0 - _TARGET) / time_to_max_speed
 	_friction = -log(1.0 - _TARGET) / time_to_zero
-	
-	var direction: Vector2 = Input.get_vector("player_left", "player_right", "player_up", "player_down")
 	
 	var velocity_weight_x: float = 1.0 - exp(-(_acceleration if direction.x else _friction) * delta)
 	velocity.x = lerp(velocity.x, direction.x * max_speed, velocity_weight_x)
 	
 	var velocity_weight_y: float = 1.0 - exp(-(_acceleration if direction.y else _friction) * delta)
 	velocity.y = lerp(velocity.y, direction.y * max_speed, velocity_weight_y)
-	
-	move_and_slide()
+
+
+func _on_dash_timer_timeout() -> void:
+	if player_state == PlayerState.DASH:
+		player_state = PlayerState.DEFAULT
 
 
 func _on_health_component_died() -> void:
+	player_state = PlayerState.DIE
 	Global.player_died.emit()
 
 func _on_health_component_health_changed(new_value: int) -> void:
