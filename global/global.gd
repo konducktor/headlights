810e7509e084140f15ea243fcb72c8202620d15e
konducktor <78530115+konducktor@users.noreply.github.com>
2026-07-23 15:18:27 +0300

    Level base

--- global/global.gd
+++ global/global.gd
@@ -9,3 +9,9 @@ signal player_regenerated(amount: int)
 signal player_max_health_changed(new_value: int)
 signal player_max_health_increased(amount: int)
 signal player_max_health_decreased(amount: int)
+
+var player: Player
+
+func _ready() -> void:
+	player = get_tree().get_nodes_in_group("Player")[0] as Player
+	print(player)
