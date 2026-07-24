c33c2fb238d4a0b8728c7a18d1376021c3e42084
Andrei Pogorelov <adpogorelov@edu.hse.ru>
2026-07-24 19:08:16 +0300

    shooter enemy added

--- enemy/fsm/states/attack.gd
+++ enemy/fsm/states/attack.gd
@@ -8,14 +8,26 @@ class_name EnemyStateAttack
 @export var max_distance: float
 @export var attack_component: AttackComponent
 
+@export var is_exit_if_no_los: bool = false
+@export var los_component: LOSComponent
+
 @export_group("Exits")
-@export var exit_if_distance_change: EnemyState
+@export var exit_if_distance_far: EnemyState
+@export var exit_if_distance_short: EnemyState
+@export var exit_if_no_los: EnemyState
 
 
 func physics_update(_delta: float) -> EnemyState:
+	if is_exit_if_no_los and not los_component.is_los_between_points(enemy.position, player.body_pos()):
+		return exit_if_no_los
+	
 	var distance: float = enemy.position.distance_to(player.body_pos())
-	if distance > max_distance or distance < min_distance:
-		return exit_if_distance_change
+	
+	if distance >= max_distance:
+		return exit_if_distance_far
+	
+	if distance <= min_distance:
+		return exit_if_distance_short
 	
 	attack_component.point_in_direction(enemy.position.direction_to(player.body_pos()))
 	attack_component.fire()
