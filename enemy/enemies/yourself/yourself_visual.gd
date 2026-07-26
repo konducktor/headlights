0d88ebd95531ab6ec61e678694e36e40aefb379b
Andrei Pogorelov <adpogorelov@edu.hse.ru>
2026-07-26 13:43:04 +0300

    boss bugfixes and home lamp

--- enemy/enemies/yourself/yourself_visual.gd
+++ enemy/enemies/yourself/yourself_visual.gd
@@ -1,8 +1,26 @@
 extends Node2D
 
 
+@export var animated_sprite: AnimatedSprite2D
 @export var animation_player: AnimationPlayer
 
 
 func _on_health_component_died() -> void:
 	animation_player.play("enemy/dissolve")
+
+
+func _on_enemy_state_chasing_entered() -> void:
+	animated_sprite.play("walking")
+
+
+func _on_enemy_state_run_entered() -> void:
+	animated_sprite.play("walking")
+
+
+func _on_knife_attack_fired() -> void:
+	animated_sprite.play("idle_attack")
+
+
+
+func _on_paper_attack_fired() -> void:
+	animated_sprite.play("idle_throw")
